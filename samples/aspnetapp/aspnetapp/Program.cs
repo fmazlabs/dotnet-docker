6a0f0fe9d59669216673fd2d238b3d144ba71d81
gregkroon <44827446+gregkroon@users.noreply.github.com>
2022-07-20 13:02:04 +1000

    Update Program.cs

--- samples/aspnetapp/aspnetapp/Program.cs
+++ samples/aspnetapp/aspnetapp/Program.cs
@@ -10,7 +10,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    //tigger test
+    //tigger test 2
     app.UseHsts();
 }
 
